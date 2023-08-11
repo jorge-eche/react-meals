1ef48e3508ae102d34b791a3f144d80c499d1af8
Jorge Echeverria <coko.eche@gmail.com>
2023-08-12 08:37:10 +1000

    conditionally add animation to Cart button

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,21 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import CartIcon from "./CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsAnimated(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ""}`;
+
   return (
-    <button
-      className={classes.button}
-      type="button"
-      onClick={props.onButtonClick}
-    >
+    <button className={btnClasses} type="button" onClick={props.onButtonClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
